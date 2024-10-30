6841a3997e3962e22af46c180676c2d1e3efa159
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2024-10-30 11:02:28 -0300

    Update utils.py

--- game/utils.py
+++ game/utils.py
@@ -2,7 +2,7 @@ import time
 import sys
 
 # Função para simular a escrita lenta na tela
-def escrever_lentamente(texto, velocidade = 0.8):
+def escrever_lentamente(texto, velocidade = 0.003):
     for char in texto:
         sys.stdout.write(char)
         sys.stdout.flush()
